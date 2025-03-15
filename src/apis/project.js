eee8bd66e822c107c827a0af237795da82512753
Dao Duc Khoa <daokhoa151202@gmail.com>
2025-03-15 10:01:03 +0700

    15/1+2

--- src/apis/project.js
+++ src/apis/project.js
@@ -9,7 +9,7 @@ export const getLstProject = async (token) => {
       },
     });
     console.log("Data retrieved successfully:", response.data);
-    return response.data;
+    return response.data.data;
   } catch (error) {
     console.error(
       "Error retrieving data:",
