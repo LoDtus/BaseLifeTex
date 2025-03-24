5eff631b09e0f9ce42c918338f4c6042bcca070d
Nguyen Hieu <nguyenminhhieu28801@gmail.com>
2025-03-24 13:56:44 +0700

    Fix

--- src/apis/project.js
+++ src/apis/project.js
@@ -1,10 +1,13 @@
-// import axios from "axios";
-import axiosInstance from "../services/apiService";
-export const getLstProject = async () => {
-  try {
-    console.log(axiosInstance);
+import axios from "axios";
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    const response = await axiosInstance.get("/projects");
+export const getLstProject = async (token) => {
+  try {
+    const response = await axios.get(`${backendUrl}/projects`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     // console.log("Data retrieved successfully:", response.data);
     return response.data.data;
   } catch (error) {
