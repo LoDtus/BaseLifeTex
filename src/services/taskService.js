4e8a8088f232fc6ec968f93927452222f093df7f
DucAnh622 <114012425+DucAnh622@users.noreply.github.com>
2025-03-27 09:46:12 +0700

    Fix call api  03/27 01

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,28 +1,28 @@
 import axiosInstance from "./apiService";
 
-const getTasks = async () => {
+export const getTasks = async () => {
   const response = await axiosInstance.get("/tasks");
   return response.data;
 };
 
-const getTasksByProject = async (projectId) => {
+export const getTasksByProject = async (projectId) => {
   const response = await axiosInstance.get(`/tasks/project/${projectId}`);
   return response.data;
 };
 
-const updateTaskStatus = async (taskId, status) => {
+export const updateTaskStatus = async (taskId, status) => {
   const response = await axiosInstance.put(`/tasks/${taskId}/status`, {
     status,
   });
   return response.data;
 };
 
-const getTaskDetailById = async (id) => {
+export const getTaskDetailById = async (id) => {
   const response = await axiosInstance.get(`/tasks/${id}`);
   return response.data;
 };
 
-const filterTask = async (idProject, data) => {
+export const filterTask = async (idProject, data) => {
   const response = await axiosInstance.post(`/tasks/filter/${idProject}`, data);
   return response.data;
 };
@@ -32,10 +32,6 @@ export const getlistUserInProjects = async (id) => {
   return response;
 };
 
-export {
-  getTasks,
-  getTasksByProject,
-  updateTaskStatus,
-  getTaskDetailById,
-  filterTask,
-};
+
+
+
