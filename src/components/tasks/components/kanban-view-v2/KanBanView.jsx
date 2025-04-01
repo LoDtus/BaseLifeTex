8295d1d5afcbf5aa69448d0382b008989dfb3788
Long Nguyen <nguyentrunglong.150903@gmail.com>
2025-04-01 16:10:12 +0700

    Update drag kanban

--- src/components/tasks/components/kanban-view-v2/KanBanView.jsx
+++ src/components/tasks/components/kanban-view-v2/KanBanView.jsx
@@ -1,11 +1,15 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Muuri from "muuri";
-import '../../styles/kanban.css'
+import '../../styles/kanban.css';
+import TaskItem from './TaskItem';
 
-export default function KanBanView() {
+export default function KanbanView() {
     const dragContainerRef = useRef(null);
     const boardRef = useRef(null);
     const columnRefs = useRef([]);
+
+    const [ openTask, setOpenTask ] = useState(null);
+    const [ checkValue, setCheckValue ] = useState([]);
 
     const columnList = [
         "Công việc mới",
@@ -40,19 +44,26 @@ export default function KanBanView() {
                         },
                     ],
                 },
+                dragStartPredicate: (item, event) => {
+                    const isCheckbox = event.target.type === "checkbox";
+                    if (isCheckbox) {
+                        return false;
+                    }
+                    return Muuri.ItemDrag.defaultStartPredicate(item, event);
+                },
             })
-                .on("dragInit", (item) => {
-                    item.getElement().style.width = `${item.getWidth()}px`;
-                    item.getElement().style.height = `${item.getHeight()}px`;
-                })
-                .on("dragReleaseEnd", (item) => {
-                    item.getElement().style.width = "";
-                    item.getElement().style.height = "";
-                    item.getGrid().refreshItems([item]);
-                })
-                .on("layoutStart", () => {
-                    boardGrid.refreshItems().layout();
-                });
+            .on("dragInit", (item) => {
+                item.getElement().style.width = `${item.getWidth()}px`;
+                item.getElement().style.height = `${item.getHeight()}px`;
+            })
+            .on("dragReleaseEnd", (item) => {
+                item.getElement().style.width = "";
+                item.getElement().style.height = "";
+                item.getGrid().refreshItems([item]);
+            })
+            .on("layoutStart", () => {
+                boardGrid.refreshItems().layout();
+            });
         });
 
         const boardGrid = new Muuri(boardRef.current, {
@@ -66,103 +77,47 @@ export default function KanBanView() {
         };
     }, []);
 
+    useEffect(() => {
+        console.log(checkValue);
+    }, [checkValue]);
+
+    useEffect(() => {
+        console.log(openTask);
+    }, [openTask]);
+
     return (
         <div>
             <div className="drag-container" ref={dragContainerRef}></div>
             <div className="board" ref={boardRef}>
                 { columnList.map((column, i) => (
-                    <div className="board-column todo">
-                        <div className="board-column-container">
-                            <div className="board-column-header">{column}</div>
-                            <div className="board-column-content-wrapper">
+                    <div className="board-column px-1 absolute top-0 left-0 bg-[#f3f4f6]"
+                        key={`col_${i}`}
+                    >
+                        <div className="board-column-container border rounded-md shadow-md" key={`col_container_${i}`}>
+                            <div className="board-column-header"
+                                key={`col_header_${i}`}>{column}</div>
+                            <div className="board-column-content-wrapper" key={`col_wrapper_${i}`}>
                                 <div
+                                    key={`col_content_${i}`}
                                     className="board-column-content"
                                     ref={(el) => (columnRefs.current[i] = el)}
                                 >
-                                    {initialTaskList.map((task, j) => (
-                                        <div
-                                            className="board-item"
-                                            key={`${i}-${task.id}`}
-                                        >
-                                            <div className="board-item-content">
-                                                <span>Item #</span>
-                                            </div>
-                                        </div>
+                                    {initialTaskList.map((task) => (
+                                        <TaskItem
+                                            key={`col_${i}-${task.id}`}
+                                            i={i}
+                                            taskKey={`col_${i}-${task.id}`}
+                                            task={task}
+                                            checkValue={checkValue}
+                                            setCheckValue={setCheckValue}
+                                            setOpenTask={setOpenTask}
+                                        />
                                     ))}
                                 </div>
                             </div>
                         </div>
                     </div>
                 ))}
-                {/* <div className="board-column todo">
-                    <div className="board-column-container">
-                        <div className="board-column-header">Todo</div>
-                        <div className="board-column-content-wrapper">
-                            <div
-                                className="board-column-content"
-                                ref={(el) => (columnRefs.current[0] = el)}
-                            >
-                                {initialTaskList.map((task, j) => (
-                                    <div
-                                        className="board-item"
-                                        key={`todo-${task.id}`}
-                                    >
-                                        <div className="board-item-content">
-                                            <span>Item </span>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="board-column working">
-                    <div className="board-column-container">
-                        <div className="board-column-header">Working</div>
-                        <div className="board-column-content-wrapper">
-                            <div
-                                className="board-column-content"
-                                ref={(el) => (columnRefs.current[1] = el)}
-                            >
-                                {initialTaskList.map((task, j) => (
-                                    <div
-                                        className="board-item"
-                                        key={`working-${task.id}`}
-                                    >
-                                        <div className="board-item-content">
-                                            <span>Item #</span>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="board-column done">
-                    <div className="board-column-container">
-                        <div className="board-column-header">Done</div>
-                        <div className="board-column-content-wrapper">
-                            <div
-                                className="board-column-content"
-                                ref={(el) => (columnRefs.current[2] = el)}
-                            >
-                                {initialTaskList.map((task, j) => (
-                                    <div
-                                        className="board-item"
-                                        key={`done-${task.id}`}
-                                    >
-                                        <div className="board-item-content">
-                                            <span>Item #</span>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
-
             </div>
         </div>
     );
--- src/layouts/components/HomeLayout.jsx
+++ src/layouts/components/HomeLayout.jsx
@@ -4,18 +4,18 @@ import NavigationBar from "@/components/navigation-bar/NavigationBar";
 import styles from "../styles/MainLayout.module.scss";
 
 export default function HomeLayout({ children }) {
-  const [searchTerm, setSearchTerm] = useState("");
-  return (
-    <div>
-      <div className={styles.header}>
-      <HomeHeader setSearchTerm={setSearchTerm} />
-      </div>
-      <div className={styles.wrapContent}>
-        <div className={styles.navbar}>
-        <NavigationBar searchTerm={searchTerm} />
+    const [searchTerm, setSearchTerm] = useState("");
+    return (
+        <div>
+            <div className={styles.header}>
+                <HomeHeader setSearchTerm={setSearchTerm} />
+            </div>
+            <div className={styles.wrapContent}>
+                {/* <div className={styles.navbar}>
+                    <NavigationBar searchTerm={searchTerm} />
+                </div> */}
+                <div className={styles.content}>{children}</div>
+            </div>
         </div>
-        <div className={styles.content}>{children}</div>
-      </div>
-    </div>
-  );
+    );
 }
