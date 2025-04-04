e2e623993824a85bad04692698ef255c646a68b1
Long Nguyen <nguyentrunglong.150903@gmail.com>
2025-04-04 11:45:11 +0700

    Merge from BaseLifeTex_Tuanta

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -114,7 +114,13 @@ export default function Home() {
             {/* Header Section */}
             <div className="header-section">
                 <div className="header-container">
-                    <div className="project-info">
+                    <div className="w-[30px] h-[30px] p-2 rounded-md
+                        cursor-pointer duration-200 hover:bg-[#f0f0f4]">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M64 144a48 48 0 1 0 0-96 48 48 0 1 0 0 96zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L192 64zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-288 0zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-288 0zM64 464a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm48-208a48 48 0 1 0 -96 0 48 48 0 1 0 96 0z"/>
+                        </svg>
+                    </div>
+                    <div className="project-info !ml-3 grow">
                         <p className="project-path">{nameProject}</p>
                     </div>
                     <div className="view-toggle">
