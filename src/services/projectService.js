aad79a75115d673cecdae09942412e5eef8fa3ca
tuantrandever <tuant4112@gmail.com>
2025-04-04 10:55:29 +0700

    tao bong bong chinh keo tha kanban

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -9,11 +9,19 @@ export const getProjectId = async (projectId) => {
   const response = await axiosInstance.get(`/projects/${projectId}`);
   return response.data;
 };
+export const postProjectId = async (projectId) => {
+  const response = await axiosInstance.post(`/projects/${projectId}`);
+  return response.data;
+};
 
 export const getMembers = async (prjId) => {
   const response = await axiosInstance.get(`/projects/${prjId}/members`);
   return response.data.data;
-}
+};
+export const getTaskMembers = async (idProject) => {
+  const response = await axiosInstance.get(`/projects/${idProject}/countTask`);
+  return response.data;
+};
 
 export const searchProjects = async (searchQuery, projectId) => {
   const response = await axiosInstance.get(`/projects/search`, {
@@ -26,4 +34,3 @@ export const deleteProjectById = async (projectId) => {
   const response = await axiosInstance.delete(`/projects/${projectId}`);
   return response.data;
 };
-
