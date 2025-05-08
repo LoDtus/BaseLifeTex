34839be4947a2cca48a2b5d0fd89019d56931cd8
TienDat-Bap <tiendat150120@gmail.com>
2025-05-08 11:24:35 +0700

    thêm ui projetccart

--- src/components/navigation-bar/components/ProjectSettingPopover.jsx
+++ src/components/navigation-bar/components/ProjectSettingPopover.jsx
@@ -27,63 +27,17 @@ const ProjectSettingPopover = ({
     >
       <DialogContent>
         <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
-          <Typography variant="h6">Tuỳ chọn dự án</Typography>
+          <Typography variant="h6">Tuỳ chọn</Typography>
           <IconButton onClick={onClose}>
             <CloseIcon />
           </IconButton>
         </Box>
 
-        <OptionItem
-          icon={<InfoOutlinedIcon />}
-          text="Xem chi tiết"
-          onClick={() => {
-            onClose();
-            onViewDetail();
-          }}
-        />
-
-        <OptionItem
-          icon={<EditOutlinedIcon />}
-          text="Chỉnh sửa"
-          onClick={() => {
-            onClose();
-            onEdit();
-          }}
-        />
-
-        <OptionItem
-          icon={<DeleteOutlineOutlinedIcon />}
-          text="Xoá"
-          color="red"
-          onClick={() => {
-            onClose();
-            onDelete();
-          }}
-        />
+        
       </DialogContent>
     </Dialog>
   );
 };
 
-const OptionItem = ({ icon, text, onClick, color }) => (
-  <Box
-    onClick={onClick}
-    sx={{
-      display: "flex",
-      alignItems: "center",
-      gap: 1.5,
-      p: 1,
-      borderRadius: 2,
-      cursor: "pointer",
-      color: color || "inherit",
-      "&:hover": {
-        backgroundColor: "#f5f5f5",
-      },
-    }}
-  >
-    {icon}
-    <Typography variant="body1">{text}</Typography>
-  </Box>
-);
 
 export default ProjectSettingPopover;
