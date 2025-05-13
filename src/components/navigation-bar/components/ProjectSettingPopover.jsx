592d85c4490003487684787683043f3efb537687
Hugn02 <29112002.hn@gmail.com>
2025-05-13 11:58:16 +0700

    crud quản lí trạng thái tạm thời trên form

--- src/components/navigation-bar/components/ProjectSettingPopover.jsx
+++ src/components/navigation-bar/components/ProjectSettingPopover.jsx
@@ -345,11 +345,11 @@ const ProjectSettingPopover = ({ onClose }) => {
                         value={addStatusValue}
                         onChange={(e) => setAddStatusValue(e.target.value)}
                         placeholder="Nhập trạng thái mới"
-                        className="flex-1 px-2 py-1 text-sm border rounded"
+                        className="flex-1 px-1 py-1 text-sm border rounded"
+                        style={{
+                          marginLeft: "-15px",
+                        }}
                       />
-                      <Button type="dashed" onClick={handleAddStatus}>
-                        Thêm
-                      </Button>
                     </li>
                   </ul>
 
